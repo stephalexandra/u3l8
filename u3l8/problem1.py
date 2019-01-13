4d42a2d99388a36d3d7a1f6aff9329142f5db594
stephalexandra <44372966+stephalexandra@users.noreply.github.com>
2019-01-12 21:27:58 -0500

    Add files via upload

---
+++ u3l8/problem1.py
@@ -0,0 +1,29 @@
+print('_' * 60)
+
+print('Morbid Predictions Program')
+
+print()
+
+print('Description: This program asks you for your current age and tells you the year that you will die. ')
+
+print()
+
+age = input('What is your age today? ')
+
+age = int(age)
+
+result = 79 - age
+
+year = result + 2018
+
+print('...thinking...')
+
+print('...thinking...')
+
+print('...thinking...')
+
+print()
+
+print('You will die in the year ' + str(year))
+
+print('_' * 70)
